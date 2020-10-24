c8780445d8ce939505c04a7efd8b570e4492f3cc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-24 10:42:46 -0500

    [/] Theme: Crea directorio src

--- functions.php
+++ functions.php
@@ -17,13 +17,13 @@ function aquila_enqueue_scripts() {
 
     # wp_enqueue_style( 'main', get_template_directory_uri(). '/main.css', [ 'style-css' ] );                   //  main.css (Ej: Carga hoja de estilos con dependencia)
     wp_enqueue_style( 'style', get_stylesheet_uri(), [], filemtime( get_stylesheet_directory() .'/style.css' ), 'all' );    //  style.csS
-    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array(), filemtime( get_stylesheet_directory() .'/assets/js/main.js' ), true );
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/src/js/main.js', array(), filemtime( get_stylesheet_directory() .'/assets/src/js/main.js' ), true );
 
     /** Registramos */
-    wp_register_style( 'archive', get_template_directory_uri(). '/assets/css/archive.css', [], filemtime( get_stylesheet_directory() .'/assets/css/archive.css' ), 'all' );
+    wp_register_style( 'archive', get_template_directory_uri(). '/assets/src/css/archive.css', [], filemtime( get_stylesheet_directory() .'/assets/src/css/archive.css' ), 'all' );
 
-    wp_register_style( 'bootstrap', get_template_directory_uri(). '/assets/library/css/bootstrap.min.css', [], '4.5.3', 'all' );               //  bootstrap.min.css
-    wp_register_script( 'bootstrap-js', get_template_directory_uri(). '/assets/library/js/bootstrap.min.js', [ 'jquery' ], '4.5.3', true );    //  bootstrap.min.js
+    wp_register_style( 'bootstrap', get_template_directory_uri(). '/assets/src/library/css/bootstrap.min.css', [], '4.5.3', 'all' );               //  bootstrap.min.css
+    wp_register_script( 'bootstrap-js', get_template_directory_uri(). '/assets/src/library/js/bootstrap.min.js', [ 'jquery' ], '4.5.3', true );    //  bootstrap.min.js
 
     wp_enqueue_style( 'bootstrap' );
     wp_enqueue_script( 'bootstrap-js' );
