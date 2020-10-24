bd3e0472b9ca9268fa622f38e60a7553ea7cf0d7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-24 11:00:56 -0500

    [+] header.php: Componente Navbar de Bootstrap usando Template-Parts

--- header.php
+++ header.php
@@ -17,4 +17,8 @@
                 wp_body_open();
             } 
         ?>
-        <header>Cabecera</header>
+        <div id="page" class="site">
+            <header id="masthead" class="site-header" role="banner">
+                <?php get_template_part( 'template-parts/header/navigation' ); ?>
+            </header>
+        </div>
