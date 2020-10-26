6aabf7c3a1dee30547a3f944800b4e212c09033e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-26 14:02:10 -0500

    [+] Theme > Class: Agrega soporte despliegue titulo dinamico

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -19,7 +19,12 @@ class Aquila_Theme {
     }
 
     protected function setup_hooks() {
-        /** Hooks: Filters/Actions */
+        /** Actions */
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+    }
+
+    public function setup_theme() {
+        add_theme_support( 'title-tag' );
     }
 
 }
