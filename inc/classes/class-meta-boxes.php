e372c1390c8c51178f1246a1ae5d91f026c91ac9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-28 17:20:01 -0500

    [+] Theme > Class > Meta_Boxes: Guarda los datos del Meta Box

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -20,6 +20,7 @@ class Meta_Boxes {
     protected function setup_hooks() {
         /** Actions */
         add_action( 'add_meta_boxes', [ $this, 'add_custom_meta_box' ] );
+        add_action( 'save_post', [ $this, 'save_post_meta_data' ] );
     }
 
     public function add_custom_meta_box( $post ) {
@@ -27,7 +28,7 @@ class Meta_Boxes {
 
         foreach ( $screens as $screen ) {
             add_meta_box(
-                'hide-page-title',                      //  ID único
+                '_hide_page_title',                     //  ID único (Metadata key)
                 __( 'Title', 'aquila' ),                //  Titulo de la caja
                 [ $this, 'custom_meta_box_html' ],      //  Content callback: Debe ser de tipo invocable
                 $screen,                                //  Post type/s
@@ -40,10 +41,10 @@ class Meta_Boxes {
         $value = get_post_meta( $post -> ID, '_hide_page_title', true );    // Obtiene un metacampo de publicación para el ID de publicación dado.
 
         ?>
-            <label for="aquila-field">
+            <label for="aquila-hide-title-field">
                 <?php esc_html_e( 'Do you want to hide the title?', 'aquila' ); ?>
             </label>
-            <select name="aquila_field" id="aquila-field" class="postbox">
+            <select name="aquila_hide_title_field" id="aquila-hide-title-field" class="postbox">
                 <option value="">
                     <?php esc_html_e( 'Select...', 'aquila' ); ?>
                 </option>
@@ -55,6 +56,16 @@ class Meta_Boxes {
                 </option>
             </select>
         <?php
+    }
+
+    public function save_post_meta_data( $post_id ) {
+        if ( array_key_exists( 'aquila_hide_title_field', $_POST ) ) {
+            update_post_meta(
+                $post_id,                               //  ID del Post
+                '_hide_page_title',                     //  ID único asignado (Metadata key)
+                $_POST[ 'aquila_hide_title_field' ]     //  Valor de metadatos. Debe ser serializable si no es escalar
+            );
+        }
     }
 
 }
--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -3,8 +3,11 @@
  * @package Aquila
  */
 
-$the_post_id = get_the_ID();                                    //  Obtiene el ID del Post
-$has_post_thumbnail = get_the_post_thumbnail( $the_post_id );   //  Obtiene la miniatura de la publicación.
+$the_post_id = get_the_ID();                                            //  Obtiene el ID del Post
+$hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );  //  Obtiene el metadato usando: ID del Post, ID único (Metadata key) y si debe devolver un solo dato o no
+$has_post_thumbnail = get_the_post_thumbnail( $the_post_id );           //  Obtiene la miniatura de la publicación.
+
+ echo '<pre>';   print_r( 'Post ID ' .$the_post_id. ' -> ' .$hide_title );  wp_die();    
 ?>
 
 <header id="entry-header">
