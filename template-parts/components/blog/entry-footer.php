d89ae9c283d5a320e3307e0ab0bd329a524cc9eb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-29 17:33:34 -0500

    [+] Template > entry-footer.php: Despliega Taxonomias de la publicacion (tanto Categorias como Etiquetas)

--- template-parts/components/blog/entry-footer.php
+++ template-parts/components/blog/entry-footer.php
@@ -1,5 +1,34 @@
 <?php
 /** Template Part: Post entry footer
+ * Usado dentro del Loop WP
  * @package Aquila
  */
+
+$post_id = get_the_ID();                            //  Obtiene el ID de una publicacion (Debe estar dentro del Loop WP)
+$article_terms = wp_get_post_terms(                 //  Recupera los términos de una publicación.
+    $post_id,                       //  ID de la Publicacion
+    [ 'category', 'post_tag' ]      //  Taxonomias
+);     
+
+// echo '<pre>';   print_r( $article_terms );  
+
+if( empty( $article_terms ) || ! is_array( $article_terms ) ) {
+    return;
+}
 ?>
+
+<div class="entry-footer mt-4">
+    <?php
+        foreach ( $article_terms as $key => $article_term ) {
+            //  get_term_link: Genera un enlace permanente para un archivo de términos de taxonomía.
+            ?>
+                <a 
+                    class="btn border border-secondary mb-2 mr-2 entry-footer-link text-black-50" 
+                    href="<?php echo esc_url( get_term_link( $article_term ) ); ?>"
+                >
+                    <?php echo esc_html( $article_term -> name ); ?>
+                </a>
+            <?php
+        }
+    ?>
+</div>
