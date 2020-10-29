7ddc572cb171ce166f3f3eb4eae079b4a6aa363a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-29 16:23:39 -0500

    [+] Template > single.php: Despliega todo el contenido de una publicacion

--- single.php
+++ single.php
@@ -13,6 +13,44 @@
             </span>
         </div>
 
+        <main id="main" class="site-main mt-5" role="main">
+            <?php 
+                if ( have_posts() ) : 
+                    ?>
+                        <div class="container">
+
+                            <?php
+                                if( is_home() && ! is_front_page() ) {
+                                    ?>
+                                        <header class="mb-5">
+                                            <h1 class="page-title screen-reader-text">
+                                                <?php single_post_title(); ?>
+                                            </h1>
+                                        </header>
+                                    <?php
+                                }
+                            ?>
+
+  
+                            <?php
+                                // Loop WP
+                                while ( have_posts() ) : the_post(); 
+                                    get_template_part( 'template-parts/content' );
+                                endwhile; 
+                            ?>
+
+
+                        </div>
+                    <?php
+                else :
+                    get_template_part( 'template-parts/content-none' );
+                endif; 
+                
+                //  Solo Debugging
+                //  get_template_part( 'template-parts/content-none' ); 
+            ?> 
+        </main>
+
     </div>
 <?php 
     get_footer();
