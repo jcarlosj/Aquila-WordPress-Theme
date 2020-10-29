5fa7a510d34884b83dbed1900cd0691027f43af7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-29 11:46:03 -0500

    [+] Template > entry-header.php: Desplegar o no el titulo de un post

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -4,5 +4,4 @@
  */
 ?>
 
-<h1><?php the_title(); ?></h1>
 <p><?php the_excerpt(); ?></p>
--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -5,9 +5,10 @@
 
 $the_post_id = get_the_ID();                                            //  Obtiene el ID del Post
 $hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );  //  Obtiene el metadato usando: ID del Post, ID único (Metadata key) y si debe devolver un solo dato o no
+$heading_class = ! empty( $hide_title ) && 'yes' === $hide_title ? 'hide' : '';     //  Valida si se debe aplicar la clase para ocultar el titulo
 $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );           //  Obtiene la miniatura de la publicación.
 
- echo '<pre>';   print_r( 'Post ID ' .$the_post_id. ' -> ' .$hide_title );  wp_die();    
+// echo '<pre>';   print_r( 'Post ID ' .$the_post_id. ' -> ' .$hide_title );  wp_die();    
 ?>
 
 <header id="entry-header">
@@ -31,5 +32,24 @@ $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );           //  Obti
                 </div>
             <?php
         }
+
+        // Title
+        if( is_single() || is_page() ) {
+            printf(
+                '<h1 class="page-title text-dark %!$s ">%2$s</h1>',
+                esc_attr( $heading_class ),
+                wp_kses_post( get_the_title() )     //  Desinfecta el contenido de las etiquetas HTML permitidas para el contenido de la publicación.
+            );
+        }
+        else {
+            printf(
+                '<h2 class="entry-title mb-3">
+                    <a class="text-dark" href="%1$s">%2$s</a>
+                </h2>',
+                esc_url( get_the_permalink() ),
+                wp_kses_post( get_the_title() )     //  Desinfecta el contenido de las etiquetas HTML permitidas para el contenido de la publicación.
+            );
+        }
+        
     ?>
 </header>
