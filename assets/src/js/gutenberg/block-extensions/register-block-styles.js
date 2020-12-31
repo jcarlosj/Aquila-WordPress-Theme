2c8e5a97e138a71237b6cf523860603593506317
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-31 16:10:32 -0500

    [+] Block Variations (quote): Crea 1 variacion adicionales de estilo de bloque

--- assets/src/js/gutenberg/block-extensions/register-block-styles.js
+++ assets/src/js/gutenberg/block-extensions/register-block-styles.js
@@ -3,21 +3,33 @@
 import { registerBlockStyle } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 
-const layoutStyleBotton = [
-    {
-        name: 'layout-border-blue-fill',
-        label: __( 'Blue outline', 'aquila' )
-    },
-    {
-        name: 'layout-border-white-no-fill',
-        label: __( 'White outline - to be used with dark background', 'aquila' )
-    }
-];
+const
+    layoutStyleBotton = [
+        {
+            name: 'layout-border-blue-fill',
+            label: __( 'Blue outline', 'aquila' )
+        },
+        {
+            name: 'layout-border-white-no-fill',
+            label: __( 'White outline - to be used with dark background', 'aquila' )
+        }
+    ],
+    layoutStyleQuote = [
+        {
+            name: 'layout-dark-background',
+            label: __( 'Layout style dark background', 'aquila' ),
+        },
+    ];
 
 const register = () => {
     layoutStyleBotton.forEach( layoutStyle => {
         registerBlockStyle( 'core/button', layoutStyle );
     });
+
+    layoutStyleQuote.forEach( ( layoutStyle ) => 
+        registerBlockStyle( 'core/quote', layoutStyle )
+	);
+    
 }
 
 /** Invocamos la funcion cuando el DOM ya se ha cargado */
